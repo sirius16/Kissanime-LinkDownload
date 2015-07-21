1549d67d4a393538af1ce76cbdd86577e956ec1a
sirius16 <amozu16@gmail.com>
2015-07-20 21:18:03 -0400

    Update kissanime.js

--- kissanime.js
+++ kissanime.js
@@ -3,11 +3,11 @@ var URL = window.location.origin;
 var hi = [];
 var hello = "";
 var episodeLinks = $('table.listing a').map(function (i, el) {
-    return $(el).attr('href');
+	return $(el).attr('href');
 });
 
 $.ajaxSetup({
-    async: false
+	async: false
 });
 $.getScript("http://kissanime.com/Scripts/asp.js");
 
@@ -17,49 +17,52 @@ var long_url;
 
 var startEpisode;
 do {
-    startEpisode = prompt("Enter episode number you want to start from");
-    if (startEpisode <= 0 || startEpisode > episodeLinks.length) {
-        alert("Episode number entered must be greater than 0 and lesser than total number of eps");
-    } else {
-        break;
-    }
+	startEpisode = prompt("Enter episode number you want to start from");
+	if (startEpisode <= 0 || startEpisode > episodeLinks.length) {
+		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
+	} else {
+		break;
+	}
 } while (true);
 
 var endEpisode;
 do {
-    endEpisode = prompt("Enter episode number you want to end at");
-    if (endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
-        alert("Episode number entered must be greater than 0 and lesser than total number of eps");
-    } else {
-        break;
-    }
+	endEpisode = prompt("Enter episode number you want to end at");
+	if (endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
+		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
+	} else {
+		break;
+	}
 } while (true);
 
 var i;
 for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEpisode); i--) {
-    jQuery.ajax({
-        url: URL + episodeLinks[i],
-        success: function (result) {
-            var $result = eval($(result));
-            var stringStart = result.search("var wra");
-            var stringEnd = result.search("document.write");
-            var javascriptToExecute = result.substring(stringStart, stringEnd);
-            eval(javascriptToExecute);
+	jQuery.ajax({
+		url: URL + episodeLinks[i],
+		success: function (result) {
+			var $result = eval($(result));
+			var stringStart = result.search("var wra");
+			var stringEnd = result.search("document.write");
+			var javascriptToExecute = result.substring(stringStart, stringEnd);
+			eval(javascriptToExecute);
 
-            $("body").append('<div id="episode' + i + '" style="display: none;"></div>');
-            $('#episode' + i).append(wra);
+			$("body").append('<div id="episode' + i + '" style="display: none;"></div>');
+			$('#episode' + i).append(wra);
 
-            var downloadQualityOptions = $('#episode' + i + ' a').map(function (i, el) {
-                return $(el);
-            });
-            hi.push(episodeLinks[i].split("/")[3].split("?")[0] + "\t" + downloadQualityOptions[0][0].href);
-        },
-        async: false,
-        script: true
-    });
+			var downloadQualityOptions = $('#episode' + i + ' a').map(function (i, el) {
+				return $(el);
+			});
+			hi.push(episodeLinks[i].split("/")[3].split("?")[0] + "\t" + downloadQualityOptions[0][0].href);
+		},
+		async: false,
+		script: true
+	});
 }
+
 function copyToClipboard(text) {
-  window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
+	window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
 }
-for (i of hi) { hello += i + "\n" }
+for (i of hi) {
+	hello += i + "\n"
+}
 copyToClipboard(hello)
