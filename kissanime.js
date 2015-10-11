2ea3a0d50d920cb290e4e86d4cd75c5a3890db00
sirius16 <amozu16@gmail.com>
2015-10-11 15:35:08 -0400

    Update kissanime.js

--- kissanime.js
+++ kissanime.js
@@ -1,7 +1,21 @@
+var s1 = document.createElement('script');
+s1.src = "http://code.jquery.com/ui/1.11.4/jquery-ui.js";
 var s = document.createElement('script');
-s.src = "http://kissanime.com/Scripts/asp.js";
+s.src = "/Scripts/asp.js";
+s1.onload = function () {
+	(document.head || document.documentElement).appendChild(s);
+}
 s.onload = function () {
-	this.parentNode.removeChild(this);
+	try {
+		$j(this).remove();
+		$j("head").append('<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">')
+		callback();
+	} catch (e) {
+		alert(e)
+	}
+}
+function callback() {
+
 	var URL = window.location.origin;
 	var hi = [];
 	var hello = [];
@@ -21,17 +35,22 @@ s.onload = function () {
 	var api_key = "R_6a13f014b38f4f80a31cf7d80a7c18c7";
 	var long_url;
 	var startEpisode;
-
+	var endEpisode;
+	$("form:gt(0)").submit(function () {
+		startEpisode = $("#start", epi).val();
+		endEpisode = $("#end", epi).val();
+		console.log(startEpisode, endEpisode)
+		return false;
+	});
 	do {
+
 		startEpisode = prompt("Enter episode number you want to start from", episodeLinks.length);
-		if (startEpisode <= 0 || startEpisode > episodeLinks.length) {
+		if (startEpisode <= 0 || startEpisode > episodeLinks.length || endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
 			alert("Episode number entered must be greater than 0 and lesser than total number of eps");
 		} else {
 			break;
 		}
 	} while (true);
-
-	var endEpisode;
 	do {
 		endEpisode = prompt("Enter episode number you want to end at", episodeLinks.length);
 		if (endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
@@ -61,10 +80,13 @@ s.onload = function () {
 					hello.push('wget -b -O "' + episodeName + '.mp4" "' + downloadQualityOptions[0][0].href + '"  --no-check-certificate');
 		});
 	}
-
-	obj(hi.join("\n"));
+$("textarea").remove()
+	if (hi.length)
+		obj(hi.join("\n"));
 	if (hello.length)
 		obj(hello.join("\n"));
+	$("<div />").append($("textarea")).dialog({})
 	alert(eval(hello.length + hi.length) + " links ready");
 };
-(document.head || document.documentElement).appendChild(s);
+
+(document.head || document.documentElement).appendChild(s1);
