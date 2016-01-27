d796bed2a80d9d229441e07d5ed0dd6704d866ca
sirius16 <amozu16@gmail.com>
2016-01-27 15:45:25 -0500

    Update kissanime.js

--- kissanime.js
+++ kissanime.js
@@ -23,9 +23,7 @@ function callback() {
 	var episodeLinks = $j('table.listing a').map(function (i, el) {
 			return $j(el).attr('href');
 		});
-	$j("table.listing a").before(function (index) {
-		return episodeLinks.length - index
-	})
+
 
 	$j.ajaxSetup({
 		async : false
